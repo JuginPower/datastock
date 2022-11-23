85e13b03ea1479cea6e571f0e1dbb73a789d11de
JuginPower <juginpower90@gmail.com>
2022-11-23 15:12:45 +0100

    refactoring: expand price method

--- app.py
+++ app.py
@@ -11,16 +11,22 @@ def ask_data():
     return indiz_object.extract_json(), 201
 
 
-@app.post("/price")
-def add_price():
-    if request.is_json:
-        from model import Price
-        price_request = request.get_json()
-        for item in price_request['item']:
-            price_object = Price(item['id'])
-            price_object + item['price']
-        return price_request, 201
-    return {"error": "Request must be JSON"}, 415
+@app.route("/price", methods=('GET', 'POST'))
+@app.route("/price/<int:id>", methods=('GET', 'POST'))
+def price(id=None):
+    from model import Price
+    if request.method == 'POST':
+        if request.is_json:
+            price_request = request.get_json()
+            for item in price_request['item']:
+                price_object = Price(item['id'])
+                price_object + item['price']
+            return price_request, 201
+        return {"error": "Request must be JSON"}, 415
+
+    elif request.method == 'GET':
+        price_object = Price(id)
+        return {'Datum': price_object.get_dates(), 'Preis': price_object.get_closes()}
 
 
 if __name__ == '__main__':
