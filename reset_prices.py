f2b07a80a518e07119beda5395f5627a5501b283
JuginPower <juginpower90@gmail.com>
2023-05-29 21:59:44 +0200

    feature-pastdata: now refreshing primary keys in price tables

--- reset_prices.py
+++ reset_prices.py
@@ -2,16 +2,28 @@ from datalayer import Datamanager
 from datetime import datetime
 
 
-prev_year = datetime.now().year-1
+create_table_string = """CREATE TABLE indiz_price{0} (
+    id int(255) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    indiz_id int(11) NOT NULL,
+    price double(10,3) NOT NULL,
+    zeit datetime NOT NULL,
+    FOREIGN KEY(indiz_id)
+    REFERENCES indiz(id)
+    );"""
+
 dm = Datamanager()
-indiz_price = dm.select(f"select indiz_id, price, zeit from indiz_price where year(zeit)={prev_year}")
+prev_year = datetime.now().year-1
+indiz_price_prev = dm.select(f"select indiz_id, price, zeit from indiz_price where year(zeit)={prev_year}")
 affected = 0
 
-if len(indiz_price) > 0:
-    affected = dm.query(f"create table indiz_price_{prev_year} as select * from indiz_price where year(zeit)={prev_year}")
-    print(affected)
-else:
-    print(f"Time: {datetime.now()}\nDid nothing")
+if len(indiz_price_prev) > 0:
 
+    affected += dm.query(create_table_string.format("_" + str(prev_year)))
+    affected += dm.query(f"insert into indiz_price_{prev_year} (indiz_id, price, zeit) values (%s, %s, %s);", indiz_price_prev)
 
-# Funktioniert aber ich muss noch reset pk aus der älteren Version auf die neu erstellte und alte Tabelle anwenden.
+    affected += dm.query(f"delete from indiz_price where year(zeit)={prev_year}")
+    indiz_price = dm.select("select indiz_id, price, zeit from indiz_price")
+    affected += dm.query("truncate indiz_price")
+    affected += dm.query("insert into indiz_price (indiz_id, price, zeit) values (%s, %s, %s);", indiz_price)
+
+print(f"Affected Rows by Transporting and resetting prices: {affected}\nPresent Time: {datetime.now()}")
