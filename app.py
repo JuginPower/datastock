654c3c2cb05a42b9d9897fca1851f5623fedd427
JuginPower <juginpower90@gmail.com>
2023-06-09 21:32:23 +0200

    feature-refactorapi: get rid of post and better indiz data call

--- app.py
+++ app.py
@@ -7,19 +7,22 @@ app = Flask(__name__)
 @app.get("/indiz")
 @app.get("/indiz/<int:id>")
 def ask_data(id=None):
-    from model import Indiz
+    from stockmodels import Indiz, Url
     indiz_object = Indiz()
-    all_data = indiz_object.extract_json()
     if id:
-        return all_data[id], 201
+        url_object = Url(id)
+        return {"id": id, 
+                "name": indiz_object.get_one_name(id),
+                "url_items": url_object.get_all_items()}, 201
     else:
-        return all_data, 201
+        return {"ids": indiz_object.get_ids(),
+                "names": indiz_object.get_names()}, 201
 
 
 @app.get("/name")
 @app.get("/name/<int:id>")
 def ask_name(id=None):
-    from model import Indiz
+    from stockmodels import Indiz
     indiz_object = Indiz()
     if id:
         return indiz_object.get_one_name(id), 201
@@ -30,7 +33,7 @@ def ask_name(id=None):
 @app.get("/id")
 @app.get("/id/<name>")
 def ask_id(name=None):
-    from model import Indiz
+    from stockmodels import Indiz
     indiz_object = Indiz()
     if name:
         return str(indiz_object.get_one_id(name)), 201
@@ -38,21 +41,21 @@ def ask_id(name=None):
         return indiz_object.get_ids(), 201
 
 
-@app.post("/price")
+# @app.post("/price")
 @app.get("/price/<int:id>")
 @app.get("/price/<int:id>/<int:amount>")
 def price(id=None, amount=None):
-    from model import Price
-    if request.method == 'POST':
+    from stockmodels import Price
+    """if request.method == 'POST':
         if request.is_json:
             price_request = request.get_json()
             for item in price_request['item']:
                 price_object = Price(item['id'])
                 price_object + item['price']
             return price_request, 201
-        return {"error": "Request must be JSON"}, 415
+        return {"error": "Request must be JSON"}, 415"""
 
-    elif request.method == 'GET':
+    if request.method == 'GET':
         if id:
             price_object = Price(id)
             if amount:
